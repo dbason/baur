c796611414896843a12564878f1f51b6640d6014
Dan Bason <dbason@diligent.com>
2019-12-03 12:50:03 +1300

    Fix init command

--- storage/postgres/init.go
+++ storage/postgres/init.go
@@ -17,7 +17,7 @@ CREATE TABLE build (
 	id SERIAL PRIMARY KEY,
 	vcs_id INTEGER REFERENCES vcs(id) ON DELETE CASCADE,
 	application_id INTEGER REFERENCES application (id) ON DELETE CASCADE,
-	branch TEST NOT NULL,
+	branch TEXT NOT NULL,
 	start_timestamp TIMESTAMP WITH TIME ZONE NOT NULL,
 	stop_timestamp TIMESTAMP WITH TIME ZONE NOT NULL,
 	total_input_digest TEXT NOT NULL
